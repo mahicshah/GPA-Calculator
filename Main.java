f09f87563efe67860aefb3608c74f3e9d592d9f1
Mahic Shah <82862036+mahicshah@users.noreply.github.com>
2022-02-19 23:00:27 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.example.gpa_calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 473);
+
+        stage.setTitle("GPA Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
